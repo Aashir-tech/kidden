7249a9f86eb2e83c10eea8d38251fbc37f9c498f
Aashir-tech <aashirharis6@gmail.com>
2025-05-20 15:54:49 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images : {
+        remotePatterns : [new URL('https://wp.dynamiclayers.net/**')],
+    }
+};
 
 export default nextConfig;
